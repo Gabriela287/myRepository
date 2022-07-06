a80b6ee85fa80a0af9a5c3f1d95a4b8b59d66b99
Gabriela Partac <gabriela.partac287@gmail.com>
2022-07-06 17:03:10 +0300

    Class of products

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,70 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Products {
+public:
+    double weight;
+    double height;
+    string TDP;
+    double nms;
+    string* memory;
+    double clk_frequency;
+    Products(double w, double  h, string tdp, double nm, string* mem, double clk_fr) {
+        weight = w;
+        height = h;
+        TDP = tdp;
+        nms = nm;
+        memory = mem;
+        clk_frequency = clk_fr;
+    }
+
+};
+
+
+class APU : public Products {
+    int noOfCores;
+    int noOfThreads;
+    string socket;
+    string* resolution;
+    enum Support_for_technology
+    {
+        OpenGL,
+        DirectX,
+    };
+    
+    APU(int cores, int threads, string soc, string* resol, Support_for_technology OpenGL, Support_for_technology DirectX) {
+        noOfCores = cores;
+        noOfThreads = threads;
+        socket = soc;
+        resolution = resol;
+        
+    }
+};
+class CPU :public Products  {
+    int noOfCores;
+    int noOfThreads;
+    string socket;
+};
+class GPU :public Products {
+    string* resolution;
+    enum Support_for_tehnology
+    {
+        OpenGL,
+        DirectX,
+    };
+};
+
+
 
 int main()
 {
-    std::cout << "fjgmjvjggkk\n";
+    CPU c;
+    c.weight = (5);
+
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
